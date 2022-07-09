32fc9bd91512ec3feb9c6d7f78bbadfc02c15b69
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-07-09 13:20:29 +0200

    Added all needed components
    At this stage i added a basic form of all the needed components, the next step is to work on each component alone.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Contact from "./components/contact/Contact";
+import Dashboard from "./components/dashboard/Dashboard";
+import Header from "./components/header/Header";
+import Profile from "./components/profile/profile";
+import Recipes from "./components/recipes/Recipes";
+import SavedRecipes from "./components/savedRecipes/SavedRecipes";
+import SignUp from "./components/signUp/SignUp";
+import Statistics from "./components/statistics/Statistics";
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello World</h1>
-  )
-}
+    <>
+      <Header />
+      <SignUp />
+      <Dashboard />
+      <Profile />
+      <Statistics />
+      <Recipes />
+      <SavedRecipes />
+      <Contact />
+    </>
+  );
+};
 
-export default App
+export default App;
