bc2d00fc64d805ce50f206769276d40ccba3d461
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-07-09 16:26:52 +0200

    Added all header elements and css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./header.css";
+import { HiHome } from "react-icons/hi";
+import { GoPerson } from "react-icons/go";
+import { IoMdStats } from "react-icons/io";
+import { BiDish } from "react-icons/bi";
+import { MdAlternateEmail } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <div className="navigation">
+        <div className="dashboard icon" title="Dashboard">
+          <HiHome />
+        </div>
+        <div className="profile icon" title="Profile">
+          <GoPerson />
+        </div>
+        <div className="statistics icon" title="Statistics">
+          <IoMdStats />
+        </div>
+        <div className="recipes icon" title="Recipes">
+          <BiDish />
+        </div>
+        <div className="contact icon" title="Contact">
+          <MdAlternateEmail />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
