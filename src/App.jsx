003cdfc76ff995a361d4773aef9c8503eb2f765b
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-07-10 13:24:50 +0200

    Changes to package.json for build options

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import Contact from "./components/contact/Contact";
 import Dashboard from "./components/dashboard/Dashboard";
 import Header from "./components/header/Header";
-import Profile from "./components/profile/profile";
+import Profile from "./components/profile/Profile";
 import Recipes from "./components/recipes/Recipes";
 import SavedRecipes from "./components/savedRecipes/SavedRecipes";
-import SignUp from "./components/signUp/SignUp";
 import Statistics from "./components/statistics/Statistics";
 
 const App = () => {
   return (
     <>
       <Header />
-      <SignUp />
       <Dashboard />
       <Profile />
       <Statistics />
