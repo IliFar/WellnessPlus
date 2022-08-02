41f5620b84a9622919ffd305ed3b8ac583acaa4d
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-02 17:14:55 +0200

    Seperating chart code to a children component

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axiosGet from "../../../api_req/statisticsReqs";
 import { Chart, registerables } from "chart.js";
 import BarChart from "../element_components/BarChart";
+import StatisticsChartUI from "./StatisticsChartUI";
+import "./Statistics.css";
 
 const Statistics = () => {
   const [nutrients, setNutrients] = useState([]);
@@ -10,33 +12,17 @@ const Statistics = () => {
     axiosGet(setNutrients);
   }, []);
 
-  Chart.register(...registerables);
-
-  const nutrientsData = {
-    labels: nutrients.map((nutrient) => nutrient.name).slice(" "),
-    datasets: [
-      {
-        label: "Vitamins",
-        backgroundColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderColor: "rgba(255,99,132,1)",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
-        data: nutrients.map((rec) => rec.DailyValue),
-      },
-    ],
-  };
   return (
     <>
-
-      <BarChart data={nutrientsData} />
+      <h1>Statistics</h1>
+      <div className="statistics">
+        <h4 className="h4">Here is your nutrients statistics RDV</h4>
+        <p className="p">Recommended Daily Value</p>
+        <div className="chart">
+        {nutrients && <StatisticsChartUI nutrients={nutrients} />}
+        </div>
+        <button>Wow</button>
+      </div>
     </>
   );
 };
