7ed08ec3c016a308479e832e2f44e79f4ea356b9
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-08 15:12:32 +0200

    Update recipesCard component

--- src/components/recipes/RecipesCard.jsx
+++ src/components/recipes/RecipesCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import "./Recipes.css"
 
 const RecipesCard = (props) => {
   return (
     <div className="recipes-card" onClick={props.onClick}>
-      <div className="title-icon">
-        <h4>{props.title}</h4>
-        <div className="recipes-icon">{props.icon}</div>
+      <div className="recipe-header">
+        <img src={props.image}/>
       </div>
-      <div className="recipes-card-body">
-        <h5>{props.action}</h5>
+      <div className="recipes-title">
+        <h5>{props.title}</h5>
       </div>
     </div>
   );
