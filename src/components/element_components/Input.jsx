07f7da891d2459a4df5ef4464c839ba0578b6cff
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-08 12:47:49 +0200

    Update input component with more props

--- src/components/element_components/Input.jsx
+++ src/components/element_components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = (props) => {
   return (
-    <input type={props.type} id={props.id} name={props.name} step={props.step} required className={props.className} value={props.value} onChange={props.onChange}/>
+    <input type={props.type} id={props.id} name={props.name} step={props.step} required className={props.className} value={props.value} onChange={props.onChange} placeholder={props.placeholder}/>
   )
 }
 
