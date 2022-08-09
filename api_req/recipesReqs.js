a09d0d44ad58bb2fd5ccdce72f4c4342b42ab90f
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-09 14:29:48 +0200

    Added get recipe by id function

--- api_req/recipesReqs.js
+++ api_req/recipesReqs.js
@@ -1,17 +1,27 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.spoonacular.com/recipes"
+const BASE_URL = "https://api.spoonacular.com/recipes";
 
 const getRecipesData = async (setData) => {
-    await axios.get(`${BASE_URL}/random?apiKey=${process.env.API_KEY}&number=9`)
+  await axios
+    .get(`${BASE_URL}/random?apiKey=${process.env.API_KEY}&number=9`)
     .then((res) => {
-        const data = res.data;
-        console.log(data);
-        setData(data.recipes);
+      const data = res.data;
+      console.log(data);
+      setData(data.recipes);
     })
     .catch((error) => {
-        console.log(error);
-    })
-}
+      console.log(error);
+    });
+};
 
-export default getRecipesData;
+const getRecipeById = async (id, setDetails) => {
+  await axios
+    .get(`${BASE_URL}/${id}/information?apiKey=${process.env.API_KEY}`)
+    .then((res) => {
+      const recipe = res.data;
+      console.log(recipe);
+      setDetails(recipe);
+    });
+};
+export { getRecipesData, getRecipeById };
