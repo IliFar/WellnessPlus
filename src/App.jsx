10d515425bab2564b9c6ed9ce041392ea75c1894
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-09 14:31:53 +0200

    Update App with context

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,40 @@ import Header from "./components/header/Header";
 import Profile from "./components/profile/Profile";
 import Recipes from "./components/recipes/Recipes";
 import SavedRecipes from "./components/savedRecipes/SavedRecipes";
+import RecipeDetails from "./components/recipes/RecipeDetails";
 import SignUp from "./components/signUp/SignUp";
 import Statistics from "./components/statistics/Statistics";
 import { getUserData } from "../api_req/signupReqs";
+import { AppContext } from "../context/appContext";
+import { getProfileData } from "../api_req/profileReqs";
 
 const App = () => {
   const [user, setUser] = useState([]);
+  const [profileData, setProfileData] = useState([]);
+  const [bmi, setBMI] = useState(null);
+  const [bmiRange, setBmiRange] = useState("");
 
   useEffect(() => {
     getUserData(setUser);
+    getProfileData(setProfileData);
   }, []);
 
   const length = user.length <= 0;
   // console.log(length);
 
   return (
-    <>
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        profileData,
+        setProfileData,
+        bmi,
+        setBMI,
+        bmiRange,
+        setBmiRange,
+      }}
+    >
       {length ? (
         <SignUp />
       ) : (
@@ -30,18 +48,19 @@ const App = () => {
             <Header />
             <div className="container d-flex flex-column justify-content-center align-items-center">
               <Routes>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/profile" element={<Profile/>}/>
-                <Route path="/statistics" element={<Statistics/>}/>
-                <Route path="/recipes" element={<Recipes/>}/>
-                <Route path="/savedrecipes" element={<SavedRecipes/>}/>
-                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/statistics" element={<Statistics />} />
+                <Route path="/recipes" element={<Recipes />} />
+                <Route path="/recipes/:id" element={<RecipeDetails />} />
+                <Route path="/savedrecipes" element={<SavedRecipes />} />
+                <Route path="/contact" element={<Contact />} />
               </Routes>
             </div>
           </Router>
         </>
       )}
-    </>
+    </AppContext.Provider>
   );
 };
 
