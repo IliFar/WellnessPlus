4b34f9e520ce4650eb86e5d157dec252f057fc99
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-10 15:25:20 +0200

    Update RecipeDetails component
    Added buttons to navigate between the recipe information ex: ingredients, and also some logic to hold a state when a button is clicked

--- src/components/recipes/RecipeDetails.jsx
+++ src/components/recipes/RecipeDetails.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRecipeById } from "../../../api_req/recipesReqs";
+import Button from "../element_components/Button";
 import RecipesCard from "./RecipesCard";
 
 const RecipeDetails = () => {
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("description");
   let params = useParams();
 
   useEffect(() => {
@@ -13,8 +15,53 @@ const RecipeDetails = () => {
 
   return (
     <>
-      <RecipesCard image={details.image} title={details.title} />
-
+      <h1>Recipe Details</h1>
+      <div className="recipe-details">
+        <RecipesCard image={details.image} title={details.title} />
+        <div className="recipe-info">
+          <div className="recipe-btns">
+            <Button
+              class={`recipe-btn ${
+                activeTab === "description" ? "active" : ""
+              }`}
+              onClick={() => setActiveTab("description")}
+            >
+              Description
+            </Button>
+            <Button
+              class={`recipe-btn ${
+                activeTab === "ingredients" ? "active" : ""
+              }`}
+              onClick={() => setActiveTab("ingredients")}
+            >
+              Ingredients
+            </Button>
+            <Button
+              class={`recipe-btn ${
+                activeTab === "instructions" ? "active" : ""
+              }`}
+              onClick={() => setActiveTab("instructions")}
+            >
+              How To Make
+            </Button>
+          </div>
+          <div className="recipe-info-d">
+            {activeTab === "description" && (
+              <p dangerouslySetInnerHTML={{ __html: details.summary }}></p>
+            )}
+            {activeTab === "instructions" && (
+              <p dangerouslySetInnerHTML={{ __html: details.instructions }}></p>
+            )}
+            {activeTab === "ingredients" && (
+              <ul>
+                {details.extendedIngredients?.map((ingredient) => (
+                  <li key={ingredient.id}>{ingredient.original}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
