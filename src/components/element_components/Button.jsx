61785d627cd4c58ee1da26feb760ea4d3860c480
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-10 12:01:55 +0200

    Add proptypes to these 4 components
    The input, the button and the barchart components are reusable components.

--- src/components/element_components/Button.jsx
+++ src/components/element_components/Button.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
 const Button = (props) => {
   return (
     <button className={props.class} type={props.type} onClick={props.onClick}>{props.children}</button>
   )
+}
+
+Button.propTypes = {
+  class: PropTypes.string,
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.string
 }
 
 export default Button
--- src/components/element_components/Input.jsx
+++ src/components/element_components/Input.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
 const Input = (props) => {
   return (
     <input type={props.type} id={props.id} name={props.name} step={props.step} required className={props.className} value={props.value} onChange={props.onChange} placeholder={props.placeholder}/>
   )
+}
+
+Input.propTypes = {
+  type: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  step: PropTypes.number,
+  className: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
 }
 
 export default Input
