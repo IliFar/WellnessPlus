4fe676d63f62975a0c8ef1db6cffc0f78ef0616a
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-12 15:27:46 +0200

    Added ProfileFormLogic component

--- src/components/profile/ProfileForm.jsx
+++ src/components/profile/ProfileForm.jsx
@@ -2,31 +2,10 @@ import React, { useEffect, useState } from "react";
 import { postProfileData } from "../../../api_req/profileReqs";
 import Button from "../element_components/Button";
 import Input from "../element_components/Input";
+import ProfileFormLogic from "./logic/ProfileFormLogic";
 
 const ProfileForm = () => {
-  const [profileForm, setProfileForm] = useState({
-    age: 0,
-    weight: 0,
-    height: 0,
-    address: "",
-  });
-
-  
-  const handleInput = (e) => {
-    const value = e.target.value;
-    const name = e.target.name;
-    setProfileForm((profileForm) => ({
-      ...profileForm,
-      [name]: value,
-    }));
-  };
-
-  useEffect(() => {
-    handleSubmit;
-  })
-  const handleSubmit = (e) => {
-    postProfileData(profileForm, setProfileForm);
-  };
+  const {profileForm, handleInput, handleSubmit} = ProfileFormLogic();
 
   return (
     <>
