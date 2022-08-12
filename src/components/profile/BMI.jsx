f54e9828a36e6828b704efe773e8900a4579406e
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-12 15:27:22 +0200

    Added BmiLogic component

--- src/components/profile/BMI.jsx
+++ src/components/profile/BMI.jsx
@@ -2,29 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { getProfileData } from "../../../api_req/profileReqs";
 import { AppContext } from "../../../context/appContext";
 import Button from "../element_components/Button";
+import BMILogic from "./logic/BMILogic";
 
 const BMI = () => {
-  const { profileData, bmi, setBMI, bmiRange, setBmiRange } =
-    useContext(AppContext);
-
-  const handleClick = () => {
-    const height = profileData.map((item) => item.height);
-    const heightInMeters = height / 100;
-    const weight = profileData.map((item) => item.weight);
-
-    const calculateBMI = weight / Math.pow(heightInMeters, 2);
-    setBMI(calculateBMI);
-
-    if (calculateBMI < 18.5) {
-      setBmiRange("Underweight");
-    } else if (calculateBMI >= 18.5 && calculateBMI <= 24.9) {
-      setBmiRange("Healthy Weight");
-    } else if (calculateBMI >= 25 && calculateBMI <= 29.9) {
-      setBmiRange("Overweight");
-    } else {
-      setBmiRange("Obese");
-    }
-  };
+  const { bmi, bmiRange, handleClick } = BMILogic();
 
   return (
     <>
