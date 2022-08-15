d1e6345e3f11d080de2b2a1a8a0de32ffb8a5bfe
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-08-15 09:57:30 +0200

    Deleted unused dependencies

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,11 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import ProfileForm from "./ProfileForm";
 import ProfileHeader from "./ProfileHeader";
 import "./Profile.css";
-import { getProfileData } from "../../../api_req/profileReqs";
 import ProfileInfo from "./ProfileInfo";
-import BMI from "./BMI";
-import { AppContext } from "../../../context/appContext";
 import ProfileLogic from "./logic/ProfileLogic";
 
 const Profile = () => {
